99fd6d56679e0bb9195cc5cfd3d779f8bbeea48c
prasad9831 <prasadshinde7015@gmail.com>
2026-08-02 23:37:03 +0530

    Fix backend CORS and frontend configuration

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -48,7 +48,6 @@ const register = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
       path: "/",
     });
@@ -101,7 +100,6 @@ const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
       path: "/",
     });
